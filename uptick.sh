1609e6199f063390ffa25b6087b0caf56509f164
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2022-11-07 16:54:34 +0200

    Create uptick.sh

---
+++ uptick.sh
@@ -0,0 +1,189 @@
+#!/bin/bash
+
+while true
+do
+
+# Logo
+
+echo -e '\e[40m\e[91m'
+echo -e '  ____                  _                    '
+echo -e ' / ___|_ __ _   _ _ __ | |_ ___  _ __        '
+echo -e '| |   |  __| | | |  _ \| __/ _ \|  _ \       '
+echo -e '| |___| |  | |_| | |_) | || (_) | | | |      '
+echo -e ' \____|_|   \__  |  __/ \__\___/|_| |_|      '
+echo -e '            |___/|_|                         '
+echo -e '    _                 _                      '
+echo -e '   / \   ___ __ _  __| | ___ _ __ ___  _   _ '
+echo -e '  / _ \ / __/ _  |/ _  |/ _ \  _   _ \| | | |'
+echo -e ' / ___ \ (_| (_| | (_| |  __/ | | | | | |_| |'
+echo -e '/_/   \_\___\__ _|\__ _|\___|_| |_| |_|\__  |'
+echo -e '                                       |___/ '
+echo -e '\e[0m'
+
+sleep 2
+
+# Menu
+
+PS3='Select an action: '
+options=(
+"Install"
+"Create Wallet"
+"Create Validator"
+"Exit")
+select opt in "${options[@]}"
+do
+case $opt in
+
+"Install")
+echo "============================================================"
+echo "Install start"
+echo "============================================================"
+
+# set vars
+if [ ! $NODENAME ]; then
+	read -p "Enter node name: " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+UPTICK_PORT=15
+if [ ! $WALLET ]; then
+	echo "export WALLET=wallet" >> $HOME/.bash_profile
+fi
+echo "export UPTICK_CHAIN_ID=uptick_7000-1" >> $HOME/.bash_profile
+echo "export UPTICK_PORT=${UPTICK_PORT}" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+# update
+sudo apt update && sudo apt upgrade -y
+
+# packages
+sudo apt install curl build-essential git wget jq make gcc tmux -y
+
+# install go
+if ! [ -x "$(command -v go)" ]; then
+  ver="1.18.2"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
+fi
+
+# download binary
+cd $HOME
+wget https://download.uptick.network/download/uptick/testnet/release/v0.2.3/v0.2.3.tar.gz --no-check-certificate
+tar -zxvf v0.2.3.tar.gz
+sudo chmod +x uptick-v0.2.3/linux/uptickd
+sudo mv uptick-v0.2.3/linux/uptickd $HOME/go/bin/
+
+# config
+uptickd config chain-id $UPTICK_CHAIN_ID
+uptickd config keyring-backend test
+uptickd config node tcp://localhost:${UPTICK_PORT}657
+
+# init
+uptickd init $NODENAME --chain-id $UPTICK_CHAIN_ID
+
+# download genesis and addrbook
+curl -o $HOME/.uptickd/config/genesis.json https://raw.githubusercontent.com/UptickNetwork/uptick-testnet/main/uptick_7000-1/genesis.json
+curl -o $HOME/.uptickd/config/addrbook.json https://raw.githubusercontent.com/kj89/testnet_manuals/main/uptick/addrbook.json
+curl -o $HOME/.uptickd/config/config.toml https://raw.githubusercontent.com/UptickNetwork/uptick-testnet/main/uptick_7000-1/config.toml
+curl -o $HOME/.uptickd/config/app.toml https://raw.githubusercontent.com/UptickNetwork/uptick-testnet/main/uptick_7000-1/app.toml
+
+# set minimum gas price
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0auptick\"/" $HOME/.uptickd/config/app.toml
+
+# set peers and seeds
+SEEDS=$(curl -sL https://raw.githubusercontent.com/UptickNetwork/uptick-testnet/main/uptick_7000-1/seeds.txt | tr '\n' ',')
+PEERS=$(curl -sL https://raw.githubusercontent.com/UptickNetwork/uptick-testnet/main/uptick_7000-1/peers.txt | tr '\n' ',')
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.uptickd/config/config.toml
+
+# disable indexing
+indexer="null"
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.uptickd/config/config.toml
+
+# config pruning
+pruning="nothing"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="50"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.uptickd/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.uptickd/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.uptickd/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.uptickd/config/app.toml
+
+# set custom ports
+sed -i.bak -e "s%^proxy_app = \"tcp://0.0.0.0:26658\"%proxy_app = \"tcp://0.0.0.0:${UPTICK_PORT}658\"%; s%^laddr = \"tcp://0.0.0.0:26657\"%laddr = \"tcp://0.0.0.0:${UPTICK_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${UPTICK_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${UPTICK_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${UPTICK_PORT}660\"%" $HOME/.uptickd/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${UPTICK_PORT}317\"%; s%^address = \":8080\"%address = \":${UPTICK_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${UPTICK_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${UPTICK_PORT}091\"%; s%^address = \"0.0.0.0:8545\"%address = \"0.0.0.0:${UPTICK_PORT}545\"%; s%^ws-address = \"0.0.0.0:8546\"%ws-address = \"0.0.0.0:${UPTICK_PORT}546\"%" $HOME/.uptickd/config/app.toml
+
+# enable prometheus
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.uptickd/config/config.toml
+
+# reset
+cd  $HOME/.uptickd
+rm -rf data
+wget -O data.tar.gz https://download.uptick.network/download/uptick/testnet/node/data/data.tar.gz --no-check-certificate
+tar -zxvf data.tar.gz
+rm -rf data.tar.gz
+
+# create service
+sudo tee /etc/systemd/system/uptickd.service > /dev/null <<EOF
+[Unit]
+Description=uptick
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which uptickd) start --home $HOME/.uptickd
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# start service
+sudo systemctl daemon-reload
+sudo systemctl enable uptickd
+sudo systemctl restart uptickd
+
+break
+;;
+
+"Create Wallet")
+uptickd keys add $WALLET
+echo "============================================================"
+echo "Save address and mnemonic"
+echo "============================================================"
+UPTICK_WALLET_ADDRESS=$(uptickd keys show $WALLET -a)
+UPTICK_VALOPER_ADDRESS=$(uptickd keys show $WALLET --bech val -a)
+echo 'export UPTICK_WALLET_ADDRESS='${UPTICK_WALLET_ADDRESS} >> $HOME/.bash_profile
+echo 'export UPTICK_VALOPER_ADDRESS='${UPTICK_VALOPER_ADDRESS} >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+break
+;;
+
+"Create Validator")
+uptickd tx staking create-validator \
+  --amount 5000000000000000000auptick \
+  --from $WALLET \
+  --commission-max-change-rate "0.01" \
+  --commission-max-rate "0.2" \
+  --commission-rate "0.07" \
+  --min-self-delegation "1" \
+  --pubkey  $(uptickd tendermint show-validator) \
+  --moniker $NODENAME \
+  --chain-id $UPTICK_CHAIN_ID \
+  --gas=auto
+  
+break
+;;
+
+"Exit")
+exit
+;;
+*) echo "invalid option $REPLY";;
+esac
+done
+done
