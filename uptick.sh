04c8e75c57eb834ff76e7e2e2a1bcd84f9bf6b8e
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2022-11-28 14:13:00 +0200

    Update uptick.sh

--- uptick.sh
+++ uptick.sh
@@ -48,14 +48,16 @@ UPTICK_PORT=15
 if [ ! $WALLET ]; then
 	echo "export WALLET=wallet" >> $HOME/.bash_profile
 fi
-echo "export UPTICK_CHAIN_ID=uptick_7000-1" >> $HOME/.bash_profile
+echo "export UPTICK_CHAIN_ID=uptick_7000-2" >> $HOME/.bash_profile
 echo "export UPTICK_PORT=${UPTICK_PORT}" >> $HOME/.bash_profile
 source $HOME/.bash_profile
+
 
 # update
 sudo apt update && sudo apt upgrade -y
 
 # packages
+
 sudo apt install curl build-essential git wget jq make gcc tmux -y
 
 # install go
@@ -71,11 +73,11 @@ source ~/.bash_profile
 fi
 
 # download binary
-curl -L -k https://github.com/UptickNetwork/uptick/releases/download/v0.2.4/uptick-linux-amd64-v0.2.4.tar.gz > uptick.tar.gz
-tar -xvzf uptick.tar.gz
-sudo mv -f uptick-linux-amd64-v0.2.4/uptickd /usr/local/bin/uptickd
-rm -rf uptick.tar.gz
-rm -rf uptick-v0.2.4
+cd $HOME
+git clone https://github.com/UptickNetwork/uptick.git
+cd uptick
+git checkout v0.2.4
+make install
 
 # config
 uptickd config chain-id $UPTICK_CHAIN_ID
@@ -86,14 +88,14 @@ uptickd config node tcp://localhost:${UPTICK_PORT}657
 uptickd init $NODENAME --chain-id $UPTICK_CHAIN_ID
 
 # download genesis and addrbook
-curl https://raw.githubusercontent.com/UptickNetwork/uptick-testnet/main/uptick_7000-1/genesis.json > $HOME/.uptickd/config/genesis.json
+curl -o $HOME/.uptickd/config/genesis.json https://raw.githubusercontent.com/UptickNetwork/uptick-testnet/main/uptick_7000-2/genesis.json
 
 # set minimum gas price
 sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0auptick\"/" $HOME/.uptickd/config/app.toml
 
 # set peers and seeds
-SEEDS=$(curl -sL https://raw.githubusercontent.com/UptickNetwork/uptick-testnet/main/uptick_7000-1/seeds.txt | tr '\n' ',')
-PEERS="fb6dba36fcf9f62a817558c0bd10e114ac8c44cc@116.203.183.189:15656,e91a765eda6a596afedc6a8407b879156177d079@161.97.99.224:26656,4f5028d78ee4f57a2ed73c57e4b2ad1f9bf9c852@75.119.151.202:30656,2763c95b0c9b0b31c312b06d6ae6887968fb9830@194.163.154.224:26656,453aff3405698476967251ee253a03bedf4f0dce@178.211.139.124:15656,5bd564b7be4fd6ec3a6bf2102dbdc09a2ce7a807@95.214.54.35:26656,9b73a0139be9c95d9a9899c7928b4db88c19e14a@209.145.62.101:15656,bf626ac1b0c733c0937d70d8c834c94c3e4b9033@65.108.129.29:14656,8c39a28b2a77ddf3dc920325b87b927eac0dac89@144.76.27.79:60656,a3db540dd85f8479d50a88e6b6aef0d6b3d39b11@135.181.35.153:26656,ecf4c3dfaa23503b62c45646a5e7229be815eba4@157.90.95.230:26656,12fe5ed38770b4bb59c59e183ec1161aebda2a4e@185.173.38.18:26656,38274db3c972fc2628eefa528930e6fb5423211f@154.53.51.219:26646,20aaf646f9c766a8b81d838554ba6e593122ed1f@46.4.122.236:36656,a9bb3d5c36cf62a280c13f3e37c93a4b17707eab@142.132.196.251:46656,12be06902509991a41c7ba809e5297e4e609453d@93.183.211.203:26656"
+SEEDS='f97a75fb69d3a5fe893dca7c8d238ccc0bd66a8f@uptick-seed.p2p.brocha.in:30554'
+PEERS=''
 sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.uptickd/config/config.toml
 
 # disable indexing
@@ -117,29 +119,27 @@ sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${U
 # enable prometheus
 sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.uptickd/config/config.toml
 
+# restore data
+cd  $HOME/.uptickd
+rm -rf data
+wget -O data.tar.gz https://download.uptick.network/download/uptick/testnet/node/data/data.tar.gz --no-check-certificate
+tar -zxvf data.tar.gz
+rm -rf data.tar.gz
+
 # create service
-sudo tee /etc/systemd/system/uptickd.service > /dev/null << EOF
+sudo tee /etc/systemd/system/uptickd.service > /dev/null <<EOF
 [Unit]
-Description=Uptick Network Node
+Description=uptick
 After=network-online.target
 [Service]
 User=$USER
-ExecStart=$(which uptickd) start
+ExecStart=$(which uptickd) start --home $HOME/.uptickd
 Restart=on-failure
-RestartSec=10
-LimitNOFILE=10000
+RestartSec=3
+LimitNOFILE=65535
 [Install]
 WantedBy=multi-user.target
 EOF
-
-uptickd tendermint unsafe-reset-all --home $HOME/.uptickd/ --keep-addr-book
-
-cd "$HOME/.uptickd" || return
-rm -rf data
-rm -rf wasm
-
-SNAP_NAME=$(curl -s https://snapshots1-testnet.nodejumper.io/uptick-testnet/ | egrep -o ">uptick_7000-1.*\.tar.lz4" | tr -d ">")
-curl https://snapshots1-testnet.nodejumper.io/uptick-testnet/${SNAP_NAME} | lz4 -dc - | tar -xf - -C $HOME/.uptickd
 
 # start service
 sudo systemctl daemon-reload
